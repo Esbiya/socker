331807f66487d3192139355071f6a0b77f6be30d
esbiya <18829040039@163.com>
2021-03-27 10:05:23 +0800

    ...

--- client_test/client.go
+++ client_test/client.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-25 13:43:56
- * @LastEditTime: 2021-03-26 19:42:50
+ * @LastEditTime: 2021-03-27 10:04:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /socker/client_test/client.go
@@ -10,13 +10,21 @@ package main
 
 import (
 	"sync"
+	"time"
 
 	"github.com/Esbiya/loguru"
 	"github.com/Esbiya/socker"
 )
 
 func main() {
-	c := socker.DefaultTCPClient()
+	c, _ := socker.NewClient(&socker.Config{
+		Mode:        socker.TCP,
+		Addr:        socker.DefaultTCPAddr,
+		InitCap:     500,
+		MaxIdle:     500,
+		MaxCap:      1 << 18,
+		IdleTimeout: 5 * time.Second,
+	})
 	defer c.Close()
 
 	c.Start()
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Esbiya/socker
 go 1.14
 
 require (
-	github.com/Esbiya/loguru v0.2.5
+	github.com/Esbiya/loguru v0.2.6
 	github.com/fatih/pool v3.0.0+incompatible
 	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/panjf2000/ants/v2 v2.4.3
